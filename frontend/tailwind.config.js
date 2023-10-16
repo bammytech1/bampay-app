e1528eb802e188e8b6ffdf0b8731b2c17df222bf
bammytech1 <66552185+bammytech1@users.noreply.github.com>
2023-10-16 10:25:50 -0700

    setting dark mode

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -5,7 +5,8 @@ export default {
   daisyui: {
     themes: [
       {
-        mytheme: {
+        light: {
+          ...require("daisyui/src/theming/themes")["[data-theme=light]"],
           primary: "#39a798",
 
           secondary: "#FFD708",
@@ -26,8 +27,62 @@ export default {
 
           error: "#f87272",
         },
+        dark: {
+          ...require("daisyui/src/theming/themes")["[data-theme=light]"],
+          primary: "#39a798",
+
+          secondary: "#FFD708",
+
+          accent: "#828282",
+
+          neutral: "#f2f2f2",
+
+          "base-200": "#f2f2f2",
+          "base-100": "#4f4f4f",
+          "base-300": "#000000",
+
+          info: "#3abff8",
+
+          success: "#39a799",
+
+          warning: "#fbbd23",
+
+          error: "#f87272",
+        },
       },
-    ],
+    ], // true: all themes | false: only light + dark | array: specific themes like this ["light", "dark", "cupcake"]
+    darkTheme: ["dark"], // name of one of the included themes for dark mode
+    base: true, // applies background color and foreground color for root element by default
+    styled: true, // include daisyUI colors and design decisions for all components
+    utils: true, // adds responsive and modifier utility classes
+    rtl: false, // rotate style direction from left-to-right to right-to-left. You also need to add dir="rtl" to your html tag and install `tailwindcss-flip` plugin for Tailwind CSS.
+    prefix: "", // prefix for daisyUI classnames (components, modifiers and responsive class names. Not colors)
+    logs: true, // Shows info about daisyUI version and used config in the console when building your CSS
+    // themes: [
+    //   {
+    //     mytheme: {
+    //       primary: "#39a798",
+
+    //       secondary: "#FFD708",
+
+    //       accent: "#828282",
+
+    //       neutral: "#f2f2f2",
+
+    //       "base-100": "#f2f2f2",
+    //       "base-200": "#4f4f4f",
+    //       "base-300": "#000000",
+
+    //       info: "#3abff8",
+
+    //       success: "#39a799",
+
+    //       warning: "#fbbd23",
+
+    //       error: "#f87272",
+    //     },
+    //   },
+    // ],
   },
   plugins: [require("daisyui")],
 };
